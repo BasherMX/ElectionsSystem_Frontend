2af6f5aa2d7977c089d8cd7d1ea73f292ec88d0d
danielpredel <danielpreciadodelgadillo@gmail.com>
2023-10-29 14:18:43 -0600

    Partyes, users done

--- src/app/admin/auth-pages/users/control/control.component.ts
+++ src/app/admin/auth-pages/users/control/control.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./control.component.less']
 })
 export class ControlComponent {
+  datos = [
+    { id: 0, nombre: 'Usuario 1', tipo: 'Verificador', status: "Inactivo"},
+    { id: 1, nombre: 'Usuario 2', tipo: 'Admin SR', status: "Activo" },
+    { id: 2, nombre: 'Usuario 3', tipo: 'Admin JR', status: "Activo" },
+    { id: 3, nombre: 'Usuario 4', tipo: 'Admin SR', status: "Activo" },
+    { id: 4, nombre: 'Usuario 5', tipo: 'Admin JR', status: "Inactivo" },
+    { id: 5, nombre: 'Usuario 6', tipo: 'Admin JR', status: "Activo" },
+    { id: 6, nombre: 'Usuario 7', tipo: 'Verificador', status: "Activo" },
+    { id: 7, nombre: 'Usuario 8', tipo: 'Admin JR', status: "Activo" },
+  ];
+  page = 1;
+  itemsPerPage = 6; // Cambia este valor según tus necesidades
+  searchText: string = '';
 
+  
+
+  editarItem(item: any) {
+    // Lógica para editar el elemento
+    console.log('Editar', item);
+  }
+
+  borrarItem(item: any) {
+    // Lógica para borrar el elemento
+    console.log('Borrar', item);
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './commonComponents/not-found/not-found.component';
+import { DashboardComponent } from './admin/auth-pages/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'admin', pathMatch: 'full' },
   { 
     path: 'admin',
     loadChildren: () => import('./admin/pages/login/login.module').then(m => m.LoginModule),
+  },
+  { 
+    path: 'admin/inicio',
+    component: DashboardComponent
+  },
+  { 
+    path: 'admin/partyes',
+    loadChildren: () => import('./admin/auth-pages/partyes/partyes.module').then(m => m.PartyesModule),
+  },
+  { 
+    path: 'admin/users',
+    loadChildren: () => import('./admin/auth-pages/users/users.module').then(m => m.UsersModule),
   },
   { 
     path: 'votation',
