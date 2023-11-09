b0c4ee70dc9887040a61ca62ca74b79e296a7d63
danielpredel <danielpreciadodelgadillo@gmail.com>
2023-11-08 19:12:40 -0600

    Flujo de Registro completado

--- src/app/admin/auth-pages/boletas/new/new.component.ts
+++ src/app/admin/auth-pages/boletas/new/new.component.ts
@@ -7,12 +7,35 @@ import { Component } from '@angular/core';
 })
 export class NewComponent {
   showModal = false;
+  modalStep = 0;
+  isRegistered = false;
+
 
   openModal(){
     this.showModal = true;
+    this.modalStep = 1;
+  }
+
+  phaseTwo(){
+    this.modalStep = 2;
+  }
+
+  phaseThree(){ 
+    this.modalStep = 3;
   }
 
   closeModal(){
     this.showModal = false;
+    this.modalStep = 0;
+  }
+
+  registerCandidate(){
+    this.isRegistered = true;
+    this.phaseTwo();
+  }
+
+  finalStep(){
+
+    this.closeModal();
   }
 }
